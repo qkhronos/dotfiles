2733bcd1949488b7399b913b3ab551add60bb353
Jaredy Capistrano Alves <qkhronos@pm.me>
2021-05-02 12:21:32 -0300

    Changing kitty and qtile theme to Solarized Dark

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,48 +1,26 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-
 from qtile_protonvpn import ProtonVpnStatus
 
 mod = "mod4"
 terminal = guess_terminal()
 
 colors = [
-    "#291f1e",
-    "#f64740",
-    "#0081a7",
-    "#00afb9",
+    "#011921",
+    "#00222D",
+    "#D1141F",
+    "#738A00",
+    "#A77800",
+    "#1A76C8",
+    "#C6156E",
+    "#209286",
+    "#EAE3CC",
 ]
- 
+
 keys = [
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
@@ -74,38 +52,23 @@ groups = [Group(i) for i in "123456789"]
 
 for i in groups:
     keys.extend([
-        # mod1 + letter of group = switch to group
         Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True), desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
     ])
-
 
 layouts = [
     layout.Columns(
-        border_focus = colors[1],
-        border_normal = colors[2],
-        border_on_single = True,
-        fair = True,
-        margin = 4,
+        border_focus=colors[2],
+        border_focus_stack=colors[2],
+        border_normal=colors[3],
+        border_normal_stack=colors[3],
+        border_on_single=True,
+        border_width=2,
+        margin=2,
+        margin_on_single=2,
     ),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
@@ -120,9 +83,16 @@ screens = [
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(
-                    borderwidth = 2,
-                    disable_drag = True,
-                    this_current_screen_border = colors[1],
+                    active=colors[8],
+                    borderwidth=2,
+                    disable_drag=True,
+                    foreground=colors[8],
+                    highlight_method='border',
+                    inactive=colors[3],
+                    this_current_screen_border=colors[2],
+                    this_screen_border=colors[2],
+                    urgent_alert_method='border',
+                    urgent_border=colors[3],
                 ),
                 widget.Prompt(),
                 widget.WindowName(),
@@ -133,13 +103,12 @@ screens = [
                 widget.Clock(format='%Y-%m-%d %a %I:%M:%S %p'),
             ],
             30,
-            background = colors[0],
-            opacity = 0.8,
+            background=colors[0],
+            opacity=0.9,
         ),
     ),
 ]
 
-# Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
@@ -147,31 +116,25 @@ mouse = [
 ]
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
+dgroups_app_rules = []
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='pinentry-gtk-2'), # GPG key password entry
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-])
+floating_layout = layout.Floating(
+    border_focus=colors[2],
+    border_normal=colors[3],
+    border_width=2,
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(title='branchdialog'),
+        Match(title='pinentry'),
+        Match(wm_class='confirmreset'),
+        Match(wm_class='makebranch'),
+        Match(wm_class='maketag'),
+        Match(wm_class='pinentry-gtk-2'),
+        Match(wm_class='ssh-askpass'),
+    ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
